6a905e9d6df771da2a1aefdfabc65bdb17154cd8
Kentaro Imai <kentaroi@kentaroimai.com>
2018-09-11 03:10:54 +0900

    Update SourceKitten to 0.21.2

--- SourceKittenWrapper/Package.swift
+++ SourceKittenWrapper/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SourceKittenWrapper",
     dependencies: [
-        .Package(url: "https://github.com/jpsim/SourceKitten.git", Version(0, 18, 1))
+        .Package(url: "https://github.com/jpsim/SourceKitten.git", Version(0, 21, 2))
     ]
 )
